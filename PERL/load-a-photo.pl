c988be00ab96041f99dcf5d3e30a0875959fae61
Dominic Caffey <DomCaf@gmail.com>
2017-12-11 15:32:30 -0700

    figured out how to display a photo

--- PERL/load-a-photo.pl
+++ PERL/load-a-photo.pl
@@ -13,6 +13,7 @@ $Data::Dumper::Sortkeys = 1;
 #$canvasHeight = 0;
 
 my $mw = MainWindow->new;
+warn "created mainwindow";
 
 #$mw->Resizeable(1,1); # Booleans for X & Y axes.
 #$mw->FullScreen;    # This is desired when everything is working smoothly.
@@ -36,42 +37,65 @@ $mw->title("$0");
 #my $qb = $gof->Button( -text => 'Quit', -command => sub { exit; } )
 #  ->pack( -side => 'bottom' );    # Quit button.
 #
-#
-#my $gdc = $mw->Canvas( -background => 'black', -borderwidth => 1, -confine => 1 )
-#  ->pack( -side => 'top', -fill => 'both', -expand => 1 );    # Game Display Canvas Widget - We're limiting scrolling to scroll region established later on.
 
-print( "Attempting to read png image grabbed from a file: \""
-      . FILE_LOC . "\"\n");
+my $gdc =
+  $mw->Canvas( -background => 'black', -borderwidth => 1, -confine => 1 )
+  ->pack( -side => 'top', -fill => 'both', -expand => 1 )
+  ; # Game Display Canvas Widget - We're limiting scrolling to scroll region established later on.
+warn "created canvas";
 
-my $thingie = $mw->Photo(
-    -format  => 'PNG',
+my $badGuyImgObj = $gdc->Photo('BadGuy',
     -file    => FILE_LOC,
+    -format  => 'PNG',
     -palette => '1/1/1'
 );
+warn "created image object of type photo";
 
-$thingie->pack(-side => 'top', -fill => 'both', -expand => 1 );
-$thingie->update();
-print "Photo, \"\" " . (($thingie->visible) ? 'visible' : 'NOT visible') . "\n";
+#$thingie->pack(-side => 'top', -fill => 'both', -expand => 1 );
+#$thingie->update();
 
-#$thingie->read($self->BAD_GUY_PNG_LOC, -format => 'PNG');
+#$mw->update();
+#print "Photo, \"\" "
+#  . ( ( $thingie->visible ) ? 'visible' : 'NOT visible' ) . "\n";
+#
+##$thingie->read($self->BAD_GUY_PNG_LOC, -format => 'PNG');
+#
+#$Data::Dumper::Sortkeys = 1;
+#print(  "BadGuy bitmap is a \""
+#      . ref($thingie)
+#      . "\" whose contents is:\n"
+#      . Dumper($thingie)
+#      . "\n" );
 
-$Data::Dumper::Sortkeys = 1;
-print( "BadGuy bitmap is a \""
-      . ref($thingie)
-      . "\" whose contents is:\n"
-      . Dumper($thingie)
-      . "\n" );
+#--------------------------------------------------------------------------------------------------
+
+# $canvas->createImage(x, y, ?option, value, option, value, ...?)
+$gdc->createImage(
+    100, 100,
+    -anchor        => 'center',
+    -image         => 'BadGuy',
+    -activeimage   => 'BadGuy',
+    -disabledimage => 'BadGuy',
+    -state         => 'normal'
+);
+warn "Created image item for canvas";
 
 $mw->update();
- 
-my $mainWindowWidth  = $mw->width;
-my $mainWindowHeight = $mw->height;
+
+my $mainWindowWidth    = $mw->width;
+my $mainWindowHeight   = $mw->height;
 my $mainWindowGeometry = $mw->geometry;
 
-print("\nMain Window Dimensions:\twidth = " . $mainWindowWidth . "\theight = " . $mainWindowHeight . "\ngeometry\t" . $mainWindowGeometry . "\n");
+warn(  "\nMain Window Dimensions:\twidth = "
+      . $mainWindowWidth
+      . "\theight = "
+      . $mainWindowHeight
+      . "\ngeometry\t"
+      . $mainWindowGeometry
+      . "\n" );
 
 MainLoop;    # This starts the graphics subsystem and causes UI to be displayed.
 
-exit 0;      #  indicate normal program termination */
+#exit 0;      #  indicate normal program termination */
 
 1;
