abb554808d9b7cad2228751f611dc3e511c3c450
Dominic Caffey <DomCaf@gmail.com>
2018-01-27 23:45:41 -0700

    Getting closer to getting Good Guy working.

--- PERL/Goodguy.pm
+++ PERL/Goodguy.pm
@@ -5,8 +5,19 @@ use Moose;
 use namespace::autoclean;
 use Data::Dumper;
 
-with 'GlobalConstants', 'MooseX::Log::Log4perl', 'Utilities';
+#with 'GlobalConstants', 'MooseX::Log::Log4perl', 'Utilities';
+with 'GlobalConstants', 'MooseX::Log::Log4perl';
 extends 'Sprites';
+
+has 'color' => (is => 'rw', isa => 'Str', default => 'blue');
+has 'gun_angle' => (is => 'rw', isa => 'Int', default => 0); # Degrees
+
+# ggbb = Good Guy Bounding Box
+has 'ggbb' => (is => 'rw', isa => 'HashRef'); # Can be populated by calling Sprites->calculateBoundingBoxCoordinates().
+
+# gbbo = Gun Bounding Box Offset.  This is how much bigger to make bounding box for drawing good guy's gun.
+#     Because good guy is round, we'll use one constant for both x & y offsets to keep things simpler.
+has 'gbbo' => (is => 'rw', isa => 'Num');
 
 # Good guy constants
 use constant {
@@ -14,26 +25,20 @@ use constant {
     GOOD_GUY_BULLET_SPEED_FACTOR => 3,
     GOOD_GUY_GUN_LENGTH          => 0.01,     #  specified as percentage 
     GOOD_GUY_RADIUS              => 0.05,     #  specified as percentage 
-    GOOD_GUY_ROTATION_INCREMENT  => 0.087,    #  5 degrees in radians 
+    GOOD_GUY_ROTATION_INCREMENT  => 5,    #  degrees
     GUN_WIDTH_HALF_ANGLE         => 0.1,      #  specified in radians 
+    GUN_WIDTH_FULL_ANGLE	=> 5,			# specified in degrees
     MAX_GOOD_GUY_EVENTS =>
       1,    #  num of events processed per visit
 
 };
 
 # Coordinates of center of good guy are in superclass Sprites.
-#my ( $x, $y );                       #  center of good guy */
 
-my ($radius);                        #  radius of good_guy, excluding barrel. implies bounding box is square.
+#my ($radius);                        #  radius of good_guy, excluding barrel. Implies bounding box is square. See ggbb
 
-my ($gun_length);                    #  length as a %age of screen dimensions */
-my ($gun_color);
+#my ($gun_length);                    #  length as a %age of screen dimensions. Handled by ggbb & gbbo.
 
-my ($gun_angle);    #  angle in radians where gun is pointing */
-my ($gun_width_half_angle)
-  ;                 #  half the angle width of the gun barrel in radians */
-
-# goodguys;
 
 # METHODS
 
@@ -119,7 +124,12 @@ sub good_guy_post_constructor {
 
     # Because good guy is circular height and width are the same.
     $self->width( GOOD_GUY_RADIUS * ( ( $maxX + $maxY ) / 2 ) );
+    $self->half_width($self->width / 2);
+
     $self->height( $self->width );
+    $self->half_height($self->height / 2);
+
+    $self->gbbo(GOOD_GUY_GUN_LENGTH * ( ( $maxX + $maxY ) / 2 ));
 
     my $success = $self->ZERO_VALUE;
 
@@ -128,80 +138,114 @@ sub good_guy_post_constructor {
     #setcolor($background_color);
     #$gdc->configure(-insertBackground => $background_color);
 
+    # Draw good guy's body.
+    my $bbc = $self->calculateBoundingBoxCoordinates();
+    $self->ggbb( $bbc );
+    $gdc->createOval( $bbc->{ul_x}, $bbc->{ul_y}, $bbc->{lr_x}, $bbc->{lr_y}, -fill => 'green', -outline => 'green');
+    $self->color( 'green' );
+
+
     $self->log->debug('Leaving good_guy_post_constructor method.');
     return ($success);
-}
+} # good_guy_post_constructor()
 
 #-----------------------------------------------------------------------------------
 
 sub draw_good_guy {
-#void draw_good_guy(players *gg, float new_angle)
 
-# DESCRIPTION     : Draws the good guy's gun turrent and gun barrel at    
-#                   its current location plus or minus the adjustment     
-#      				 angle that is passed to it.  Also makes the adjustment
-#                   in the good guy's data structure as to what its       
-#                   current gun angle is.                                 
-# INPUTS          : A ref to Game Display Canvas and the gun barrel adjustment
-#                   angle specified in radians.                           
-# OUTPUTS         : The good guy's new gun angle via the parameter list.  
+    #void draw_good_guy(players *gg, float new_angle)
+
+  # DESCRIPTION     : Draws the good guy's gun turrent and gun barrel at
+  #                   its current location plus or minus the adjustment
+  #      				 angle that is passed to it.  Also makes the adjustment
+  #                   in the good guy's data structure as to what its
+  #                   current gun angle is.
+  # INPUTS          : A ref to Game Display Canvas and the gun barrel adjustment
+  #                   angle specified in radians.
+  # OUTPUTS         : The good guy's new gun angle via the parameter list.
 
     my $self = shift;
 
-    $self->log->debug( 'Entered draw_good_guy method.');
+    $self->log->debug('Entered draw_good_guy method.');
 
-    my $gdc  = shift;    # Game Display Canvas object = gdc.
-	my $new_angle = shift; # Gun barrel adjustment angle for good guy in radians.
+    my $gdc = shift;    # Game Display Canvas object = gdc.
+    my $new_angle =
+      shift;            # Gun barrel adjustment angle for good guy in radians.
 
     my $maxX    = $gdc->cget( -width );
     my $maxY    = $gdc->cget( -height );
     my $success = $self->ZERO_VALUE;
 
-	my $background_color = $gdc->cget(-background);
+    my $background_color = $gdc->cget( -background );
 
-	#setcolor($background_color);
-	$gdc->configure(-insertBackground => $background_color);
+    #setcolor($background_color);
+    #$gdc->configure( -insertBackground => $background_color );
 
-	# erase the gun barrel from its current location -----------------------------------------------------------
+# erase the gun barrel from its current location -----------------------------------------------------------
 
-	#setfillstyle(SOLID_FILL,$background_color);
+    #setfillstyle(SOLID_FILL,$background_color);
 
-	$radius = GOOD_GUY_RADIUS * (($maxX + $maxY) / 2); # put in Sprites superclass?
-	$gun_angle = $new_angle;
+#    $radius =
+#      GOOD_GUY_RADIUS * ( ( $maxX + $maxY ) / 2 );  # put in Sprites superclass?
+#    $gun_angle = $new_angle;
+#
+#    my $x_barrel =
+#      $self->polar_to_cartesian_coords( $radius, $gun_angle, 'x' ) + $self->x;
+#    my $y_barrel =
+#      $self->polar_to_cartesian_coords( $radius, $gun_angle, 'y' ) + $self->y;
+#    my $gun_length = GOOD_GUY_GUN_LENGTH * ( ( $maxX + $maxY ) / 2 )
+#      ;    # Put in Sprites superclass? No because specific to good guy.
 
-	my $x_barrel = $self->polar_to_cartesian_coords($radius,$gun_angle,'x') + $self->x;
-	my $y_barrel = $self->polar_to_cartesian_coords($radius,$gun_angle,'y') + $self->y;
-	my $gun_length = GOOD_GUY_GUN_LENGTH * (($maxX + $maxY) / 2); # put in Sprites superclass?
+    my $bbc =
+      $self->ggbb;   # Get good guy's bounding box coordinates as a hashref.
 
-	# fillellipse(x_barrel,y_barrel,$gun_length,$gun_length);
-	# It might be easiest to have a bounding box for the gun barrel slightly larger than the good guy
-	# body centered on the same point as the good guy body. Store the good guy body color in good guy
-	# object and use same color for drawing its gun barrel.
+    $gdc->createArc(
+        ( $bbc->{ul_x} - $self->gbbo ),
+        ( $bbc->{ul_y} - $self->gbbo ),
+        ( $bbc->{lr_x} + $self->gbbo ),
+        ( $bbc->{lr_y} + $self->gbbo ),
+	-start => $self->gun_angle,
+	-extent => GUN_WIDTH_FULL_ANGLE,
+	-fill => 'black',
+	-outline => 'black',
+	-style => 'pieslice'
+    );
 
-	 $gdc->createOval($x_barrel,$y_barrel,$gun_length,$gun_length);
+# It might be easiest to have a bounding box for the gun barrel slightly larger than the good guy
+# body centered on the same point as the good guy body. Store the good guy body color in good guy
+# object and use same color for drawing its gun barrel.
 
-	#  redraw gun barrel at its new location -----------------------------------------------------------
+    #$gdc->createOval( $x_barrel, $y_barrel, $gun_length, $gun_length );
 
-	setfillstyle(SOLID_FILL,gg->pd.gg.gun_color);
-	gg->pd.gg.gun_angle+= new_angle;
+#  redraw gun barrel at its new location -----------------------------------------------------------
 
-	x_barrel = (int) polar_to_cartesian_coords(gg->pd.gg.radius,gg->pd.gg.gun_angle,'x') + gg->pd.gg.x;
-	y_barrel = (int) polar_to_cartesian_coords(gg->pd.gg.radius,gg->pd.gg.gun_angle,'y') + gg->pd.gg.y;
-
-	fillellipse(x_barrel,y_barrel,gg->pd.gg.gun_length,gg->pd.gg.gun_length);
+#    setfillstyle( SOLID_FILL, gg->pd . gg . gun_color );
+#    gg->pd . gg . gun_angle += new_angle;
+#
+#    x_barrel = polar_to_cartesian_coords( gg->pd . gg . radius,
+#        gg->pd . gg . gun_angle, 'x' ) + gg->pd
+#      . gg
+#      . x;
+#    y_barrel = polar_to_cartesian_coords( gg->pd . gg . radius,
+#        gg->pd . gg . gun_angle, 'y' ) + gg->pd
+#      . gg . y;
+#
+#	createArc(x_barrel,y_barrel,gg->pd.gg.gun_length,gg->pd.gg.gun_length);
 
 	#  redraw the gun turret body - maybe this should only be done when a hit is taken */
-	# fillellipse(gg->pd.gg.x,gg->pd.gg.y,gg->pd.gg.radius,gg->pd.gg.radius);
+	# createArc(gg->pd.gg.x,gg->pd.gg.y,gg->pd.gg.radius,gg->pd.gg.radius);
 
-    $self->log->debug('Leaving draw_good_guy method.');
+## Please see file perltidy.ERR
+## Please see file perltidy.ERR
+      $self->log->debug('Leaving draw_good_guy method.');
 
     return ($success);
-	}
+}    # draw_good_guy()
 
 # ****************************< End draw_good_guy >***********************/
 
 
-players * add_good(void);
+#players * add_good(void);
 
 
 1;
--- PERL/Sprites.pm
+++ PERL/Sprites.pm
@@ -9,12 +9,15 @@ use namespace::autoclean;
 
 use constant { BULLET_RADIUS => 2 };
 
+#with 'GlobalConstants', 'MooseX::Log::Log4perl', 'Utilities';
+with 'MooseX::Log::Log4perl';
+
 has 'bitmap' => ( is => 'rw', isa => 'Object' );
 
 # Because Tk graphics primitives generally rely on bounding boxes for drawing a lot of things, it might
 # make more sense to store the bounding box coordinates of upper-left and lower-right points then the
 # center of a graphic entity, and its width and height. On the other hand, the math will get a lot more
-# complicated so it might be best to utility create a routine that calculates the bounding box points based on
+# complicated so it might be best to create a utility  routine that calculates the bounding box points based on
 # the center point coordinates and height & width info below.
 
 has 'x' => ( is => 'rw', isa => 'Num' );
@@ -22,7 +25,46 @@ has 'x' => ( is => 'rw', isa => 'Num' );
 has 'y' => ( is => 'rw', isa => 'Num' );
 
 has 'width' => ( is => 'rw', isa => 'Num' );
+has 'half_width' => ( is => 'rw', isa => 'Num' );
 
 has 'height' => ( is => 'rw', isa => 'Num' );
+has 'half_height' => ( is => 'rw', isa => 'Num' );
+
+sub calculateBoundingBoxCoordinates {
+
+# This subroutine/method calculates the bounding box coordinates so that a bitmap or graphics object can be drawn
+# due to Tk's requirement that many graphics primitives require a bounding box to be specified to do things. This
+# bounding box is specified using (x,y) coordinates for the points for the upper left and lower right corners of
+# the bounding box. These calculations are based on the equation for a line i.e. y = mx + b.
+
+# Also note that when calculating the new position of a sprite, one should update this object instance's x & y center
+# point coordinates BEFORE calling this method.
+
+# This method returns a reference to a hash containing the calculated bounding box coordinates.
+
+    my $self = shift;
+
+    $self->log->debug('Entered calculateBoundingBoxCoordinates method.');
+
+    my $b = $self->y;
+
+    my $m =
+      ( $self->height == $self->width ) ? 1 : ( $self->height / $self->width );
+
+    my %boundingBoxCoordinates = ();
+
+    # ul = upper left of bounding box
+    # lr = lower right of bounding box
+
+    $boundingBoxCoordinates{ul_x} = $self->x - $self->half_width;
+    $boundingBoxCoordinates{ul_y} = $self->y - $self->half_height;
+    $boundingBoxCoordinates{lr_x} = $self->x + $self->half_width;
+    $boundingBoxCoordinates{lr_y} = $self->y + $self->half_height;
+
+    $self->log->debug('Leaving calculateBoundingBoxCoordinates method.');
+
+    return ( \%boundingBoxCoordinates );
+
+}    # calculateBoundingBoxCoordinates()
 
 1;
